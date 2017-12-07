de693db60be000a0e1861ab4f272163c484705d1
mcelvain <mcelvain@iastate.edu>
2017-12-07 16:36:46 -0600

    MERGING AGAIN

--- src/main/groovy/web/App.groovy
+++ src/main/groovy/web/App.groovy
@@ -5,7 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-
+import org.springframework.context.annotation.*;
 
 @Configuration
 @EnableAutoConfiguration
